3b636119e88c219d87606958767dac3299965cab
Egor Lysenko <uprendis@gmail.com>
2023-10-20 02:06:19 +0400

    replace mnemonics with keystore

--- cmd/govservant/main.go
+++ cmd/govservant/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,6 +9,7 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -15,64 +17,52 @@ func isHelp(args []string) bool {
 	return len(os.Args) >= 2 && (os.Args[1] == "-h" || os.Args[1] == "-help" || os.Args[1] == "--help" || os.Args[1] == "help")
 }
 
+func readKey(keyPath, passPath string) (common.Address, *ecdsa.PrivateKey) {
+	keyjson, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Private key reading error: %v\n", err)
+		os.Exit(1)
+		return common.Address{}, nil
+	}
+	// read first line of the password file
+	authText, err := ioutil.ReadFile(passPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Password file reading error: %v\n", err)
+		os.Exit(1)
+		return common.Address{}, nil
+	}
+	auth := strings.Split(string(authText), "\n")[0]
+
+	key, err := keystore.DecryptKey(keyjson, auth)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Private key decrypting error: %v\n", err)
+		os.Exit(1)
+		return common.Address{}, nil
+	}
+	return key.Address, key.PrivateKey
+}
+
 func main() {
-	if len(os.Args) != (1+4) || isHelp(os.Args) {
-		fmt.Printf("Usage: %s /path/to/funder-mnemonic /path/to/stolen-mnemonic destination-addr RPC_URL\n", os.Args[0])
+	if len(os.Args) != (1+6) || isHelp(os.Args) {
+		fmt.Printf("Usage: %s /path/to/funder-key /path/to/funder-pass /path/to/stolen-key /path/to/stolen-pass destination-addr RPC_URL\n", os.Args[0])
 		return
 	}
 	// parse arguments
-	funderMnemonicPath := os.Args[1]
-	stolenMnemonicPath := os.Args[2]
-	if !common.IsHexAddress(os.Args[3]) {
+	funderKeyPath := os.Args[1]
+	funderPassPath := os.Args[2]
+	stolenKeyPath := os.Args[3]
+	stolenPassPath := os.Args[4]
+	if !common.IsHexAddress(os.Args[5]) {
 		fmt.Fprintf(os.Stderr, "Destination address parsing error\n")
 		return
 	}
 	destinationAddr := common.HexToAddress(os.Args[3])
-	rpcUrl := os.Args[4]
+	rpcUrl := os.Args[6]
 
 	// decrypt the private key
 
-	funderMnemonic, err := ioutil.ReadFile(funderMnemonicPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Funder mnemonic reading error: %v\n", err)
-		return
-	}
-	stolenMnemonic, err := ioutil.ReadFile(stolenMnemonicPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Stolen mnemonic reading error: %v\n", err)
-		return
-	}
-	funderSplit := strings.Split(string(funderMnemonic), "->")
-	if len(funderSplit) != 2 {
-		fmt.Fprintf(os.Stderr, "Mnemonic must have format: addr -> mnemonic-words\n", err)
-		return
-	}
-	stolenSplit := strings.Split(string(stolenMnemonic), "->")
-	if len(stolenSplit) != 2 {
-		fmt.Fprintf(os.Stderr, "Mnemonic must have format: addr -> mnemonic-words\n", err)
-		return
-	}
-	if !common.IsHexAddress(strings.TrimSpace(funderSplit[0])) {
-		fmt.Fprintf(os.Stderr, "Funder address parsing error\n")
-		return
-	}
-	funderAddr := common.HexToAddress(strings.TrimSpace(funderSplit[0]))
-	if !common.IsHexAddress(strings.TrimSpace(stolenSplit[0])) {
-		fmt.Fprintf(os.Stderr, "Stolen address parsing error\n")
-		return
-	}
-	stolenAddr := common.HexToAddress(strings.TrimSpace(stolenSplit[0]))
-
-	funderKey := privateKeyFromMnemonic(strings.TrimSpace(funderSplit[1]), funderAddr)
-	if funderKey == nil {
-		fmt.Fprintf(os.Stderr, "Failed to derive funder key\n")
-		return
-	}
-	stolenKey := privateKeyFromMnemonic(strings.TrimSpace(stolenSplit[1]), stolenAddr)
-	if funderKey == nil {
-		fmt.Fprintf(os.Stderr, "Failed to derive stolen key\n")
-		return
-	}
+	funderAddr, funderKey := readKey(funderKeyPath, funderPassPath)
+	stolenAddr, stolenKey := readKey(stolenKeyPath, stolenPassPath)
 
 	// Governance maintenance
 
